f4102c41c2c9844584ecd6a7968f6395aae4006b
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2025-02-25 18:15:04 -0300

    rds con dos subnet a e b

--- aws_db_instance.tf
+++ aws_db_instance.tf
@@ -7,7 +7,6 @@ resource "aws_db_instance" "bia" {
   allow_major_version_upgrade           = null
   apply_immediately                     = null
   auto_minor_version_upgrade            = true
-  availability_zone                     = "us-east-1a"
   backup_retention_period               = 1
   backup_window                         = "08:32-09:02"
   ca_cert_identifier                    = "rds-ca-rsa2048-g1"
@@ -60,4 +59,14 @@ resource "aws_db_instance" "bia" {
   timezone                              = null
   username                              = "postgres"
   vpc_security_group_ids                = [aws_security_group.bia_db.id]
+  db_subnet_group_name = aws_db_subnet_group.bia.name
+}
+
+resource "aws_db_subnet_group" "bia" {
+  name = "bia-subnet-group"
+  subnet_ids = [local.subnet_zona_a, local.subnet_zona_b]
+
+  tags = {
+    name="bia-subnet-group"
+  }
 }
--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   vpc_id="vpc-0c5f464e37ffc5d93"
   subnet_zona_a ="subnet-0ff4c0dcbb0ea82ed"
+  subnet_zona_b ="subnet-05c95bdad75682453"
 }
